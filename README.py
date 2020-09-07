a5610ecabbe4d7bdeae7ac98823cd6c5f1c3c1c7
lagoamiguel <64663910+lagoamiguel@users.noreply.github.com>
2020-09-07 08:50:31 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,7 @@
 # Practice
 
+
+
 import torch 
 
 # A
@@ -7,10 +9,10 @@ class Perceptron(torch.nn.Module):
     def __init__(self):
         super(Perceptron, self).__init__()
         self.fc = nn.Linear(1,1)
-        self.relu = torch.nn.ReLU() # instead of Heaviside step fn
+        self.relu = torch.nn.ReLU()
     def forward(self, x):
         output = self.fc(x)
-        output = self.relu(x) # instead of Heaviside step fn
+        output = self.relu(x) 
         return output
 
 # B
@@ -24,6 +26,7 @@ class Feedforward(torch.nn.Module):
             self.relu = torch.nn.ReLU()
             self.fc2 = torch.nn.Linear(self.hidden_size, 1)
             self.sigmoid = torch.nn.Sigmoid()
+            
         def forward(self, x):
             hidden = self.fc1(x)
             relu = self.relu(hidden)
@@ -31,4 +34,39 @@ class Feedforward(torch.nn.Module):
             output = self.sigmoid(output)
             return output
             
-# C
+# C ... i dont understad this code enough to know how to add a second layer(circle back)
+            
+class Feedforward(torch.nn.Module):
+        def __init__(self, input_size, hidden_size):
+            super(Feedforward, self).__init__()
+            self.input_size = input_size
+            self.hidden_size  = hidden_size
+            self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+            self.relu = torch.nn.ReLU()
+            self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+            self.sigmoid = torch.nn.Sigmoid()
+            self.fc3 = torch.nn.Linear(self.input_size, self.hidden_size)
+            self.relu = torch.nn.ReLU()
+            self.fc4 = torch.nn.Linear(self.hidden_size, 1)
+            self.sigmoid = torch.nn.Sigmoid()
+            
+        def forward(self, x):
+            hidden = self.fc1(x)
+            relu = self.relu(hidden)
+            output = self.fc2(relu)
+            output = self.sigmoid(output)
+            return output
+        
+        def forward(self, x):
+            hidden = self.fc3(x)
+            relu = self.relu(hidden)
+            output = self.fc4(relu)
+            output = self.sigmoid(output)
+            return output
+        
+# D
+            
+        
+        
+        
+        
